924a1b0a91ca3cfac6df88f4d33e15debe8b85ce
chisato <chisato-x@bupt.edu.cn>
2025-02-21 22:00:00 +0800

    1. define value type

--- pkg/storage_api/gorm_api/user_tenant_mixed_cfg_test.go
+++ pkg/storage_api/gorm_api/user_tenant_mixed_cfg_test.go
@@ -1,6 +1,7 @@
 package gorm_api
 
 import (
+	"encoding/json"
 	"github.com/Juminiy/kube/pkg/util"
 	"github.com/Juminiy/kube/pkg/util/encrypt"
 	"gorm.io/gorm"
@@ -74,4 +75,27 @@ func TestSelectOmit(t *testing.T) {
 	var userList []AppUser
 	Err(t, _txTenant().Find(&userList).Error)
 	t.Log(Enc(userList))
+}
+
+type Name string
+
+func (n Name) MarshalJSON() ([]byte, error) {
+	if n == "" || n == "null" {
+		return nil, nil
+	}
+	return util.String2BytesNoCopy(string(n)), nil
+}
+
+func (n *Name) UnmarshalJSON(b []byte) error {
+	if str := util.Bytes2StringNoCopy(b); str != "null" {
+		*n = Name(str)
+	}
+	return nil
+}
+
+func TestMagicType(t *testing.T) {
+	n := Name("null")
+	bs, err := json.Marshal(n)
+	Err(t, err)
+	t.Logf("%s", bs)
 }
